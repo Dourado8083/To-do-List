4c56bfac8b9b65e9cbf0fc129cef614f85079f42
Gustavo Dourado <gustavo.dourado.br@gmail.com>
2023-01-30 21:09:32 -0300

    Criação do adicionar|deletar tarefa e validações de vazio e espaçamento

--- src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
+++ src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-todo-input-add-itens',
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./todo-input-add-itens.component.scss']
 })
 export class TodoInputAddItensComponent {
+@Output() public emitItemTaskList = new EventEmitter();
+
+public addItemTaskList: string = '';
+
+public submitTaskList(){
+  //trim remove os espaços
+  this.addItemTaskList = this.addItemTaskList.trim();
+  if(this.addItemTaskList){
+  /* console.log(this.addItemTaskList); */
+  this.emitItemTaskList.emit(this.addItemTaskList);
+  this.addItemTaskList = '';
+ }
+}
 
 }
--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -5,17 +5,31 @@ import { TaskList } from '../../model/task-list';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent {
-public taskList: Array<TaskList> = [
-/*   {
-task:"Minha Nova Task",checked:true
-},
-{
-  task:"Minha Nova Task 2",checked:false
-  } */
+  public taskList: Array<TaskList> = [
+/*     {
+      task: 'Minha Nova Task',
+      checked: true,
+    },
+    {
+      task: 'Minha Nova Task 2',
+      checked: false,
+    }, */
+  ];
+  
+ public setEmitTaskList(event: string){
+ this.taskList.push({task: event, checked:false});
+ }
 
-];
-
+  public deleteItemTaskList(event: number) {
+    this.taskList.splice(event, 1);
+  }
+  public deleteAllTaskList() {
+    const confirm = window.confirm('Você deseja deletar tudo??');
+    if (confirm) {
+      this.taskList = [];
+    }
+  }
 }
